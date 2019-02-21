ff9ea49b7e728692c3245eb7dd29b984fe0bfc05
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-20 19:21:16 -0500

    cleaned it up, commented the crap out of it, but still having issues with some things in the output

--- main.py
+++ main.py
@@ -1,58 +1,40 @@
+#Import cssutils and webcolors as required
 import cssutils as cu
 import webcolors as wc
 
+#Found this online, it suppresses the ridiculous level of error logging from cssutils
 import logging
 cu.log.setLevel(logging.CRITICAL)
 
-# print(css_file)
-
-
-blue = 'blue'
-blurb = 'blurb'
+#Giving a shorter name to the name-to-hex dictionary
 nth = wc.CSS2_NAMES_TO_HEX
 
+#I'll keep the next few lines in for now, shows how I figured out how to only
+#convert a color value to hex if there's a hex representation for that value
+# blue = 'blue'
+# blurb = 'blurb'
 # if blue in nth:
 #     print(nth[blue])
 # if blurb in nth:
 #     print(nth[blurb])
 
+#Read from the css file provided
+inbound_file = cu.parseFile('main.css')
 
-sheet = cu.parseFile('main.css')
-
-# for rule in sheet:
-#     print(rule.cssText)
-
-for rule in sheet:
+#For each rule in our file...
+for rule in inbound_file.cssRules:
+    #If it's a style rule...
     if rule.type == rule.STYLE_RULE:
-        # find property
+        #Then check out the properties it has
         for property in rule.style:
-            if property.name == 'color' and property.value in nth:
-                # print(property.value)
+            #And if it's either color or background-color and has a name-to-hex value
+            if (property.name == 'color' or property.name == 'background-color') and property.value in nth:
+                #Then change it's value to the hex value
                 property.value = nth[property.value]
-                # property.priority = 'IMPORTANT'
-                break
-        # or simply:
-        # rule.style['margin'] = '01.0eM' # or: ('1em', 'important')
 
-# for rule in sheet:
-#     print(rule.cssText)
-
-print('hi')
-# print(sheet.ru)
-    # if 'color' in rule.:
-    #     rule.color = nth[rule.color]
-output_file = open("output.css","w")
-# print(str(sheet.cssText))
-output_file.write(sheet.cssText.decode("utf-8"))
-output_file.close()
-
-
-
-
-# print(rule)
-# print(rule.style)
-# rule.style.width = '90%'
-# print(rule)
-# sheet = cssutils.parseString('@import url(main.css)')
-# print('hello!')
-# print(sheet)
+#Get a new file ready to write to
+outbound_file = open("new_main.css","w")
+#Decode the bytestring values of the outbound file and encode as UTF-8
+outbound_file.write(inbound_file.cssText.decode("utf-8"))
+#Finally, close the file
+outbound_file.close()
